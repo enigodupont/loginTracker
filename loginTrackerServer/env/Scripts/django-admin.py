2e8f1111a93aedfc5d519f0c090004c719d9151b
ramirezj <ramirezj@Eden.localdomain>
2017-09-30 15:03:17 -0500

    Init Repo

---
+++ loginTrackerServer/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jc648\source\repos\loginTrackerServer\loginTrackerServer\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
