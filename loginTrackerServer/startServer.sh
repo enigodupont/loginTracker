9068ede2c889b3ba6c4a21897a34aa9f2ac07ae5
Juan Carlos Ramirez <jc6482@yahoo.com>
2018-02-14 21:10:28 -0600

    Modified start script and made new linux ENV

--- loginTrackerServer/startServer.sh
+++ loginTrackerServer/startServer.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+
+if [  $(pgrep startloginTrackerServer.sh) ]
+then
+  echo "Another script is running, lets kill it...."
+  kill $(pgrep startloginTrackerServer.sh)
+  sleep 1
+fi
+
+if [[ $(pgrep -f 'runserver.*4242') ]]
+then
+  echo "Another server is running, lets kill it...."
+  kill $(pgrep -f "runserver.*4242")
+  sleep 1
+fi
+
 source ./linuxEnv/bin/activate
-./manage.py runserver 0.0.0.0:4242
+python3 ./manage.py runserver 0.0.0.0:4242
