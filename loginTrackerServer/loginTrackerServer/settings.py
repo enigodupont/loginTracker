f406c03cf6f77753a341815d4895eb9d2442e6bf
Juan Carlos Ramirez <juramir@protonmail.com>
2022-05-13 08:18:53 -0500

    Upgrade to django 3.2

--- loginTrackerServer/loginTrackerServer/settings.py
+++ loginTrackerServer/loginTrackerServer/settings.py
@@ -89,6 +89,9 @@ except ImportError:
         }
     }
 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
